8bfa8db3682937e9db5dda1a98f071879507b0c9
Tursin-xD <anuartursyn88@gmail.com>
2026-04-25 11:14:49 +0500

    Update main.py

--- main.py
+++ main.py
@@ -44,6 +44,7 @@ async def get_info(query, is_url=False):
                 }
             },
             # Add a real User-Agent so YouTube doesn't think you're a bot script
+            #restart here
             'user_agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
             'ignoreerrors': True,
         }
